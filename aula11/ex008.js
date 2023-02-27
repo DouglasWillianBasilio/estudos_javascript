c4b749ff969be6459ef843a4a80e36a022ee8d8c
Douglas1301 <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-02-27 14:47:25 -0300

    atualizações

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 60.5
+
+console.log(`A velocidade do carro é ${vel} hm/h`)
+
+if( vel > 60){
+    console.log("Voce foi multado!")
+} 
+else{
+    console.log("Voce esta na velocidade correta")
+}
