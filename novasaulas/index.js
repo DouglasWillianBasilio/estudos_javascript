58305b55ebab995edf407dd043896e309be0863b
Douglas1301 <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-03-06 15:40:07 -0300

    JS

---
+++ novasaulas/index.js
@@ -0,0 +1,2 @@
+document.getElementById("texto").innerHTML = "Meu Primeiro texto <b>Js</b>"; /*PUXEI O ID TEXTO DO HTML E DEI UMA SAIDA PARA ELE IMPRIMIR ALGO*/
+/*innerHTML foi usado para inserir o conteudo ao ID*/
